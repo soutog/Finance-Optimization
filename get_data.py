76f39c5af845d73408e370762c62b9248db5f058
Gabriel Souto <soutog4@gmail.com>
2023-07-08 07:47:15 -0300

    new data structure tickers in rows and indicators in columns

--- get_data.py
+++ get_data.py
@@ -52,22 +52,57 @@ class GetData:
         # Stocks removed if data of the stock is not complete
         df_stock = df_stock.dropna(axis=1)
 
+        df_stock.reset_index(inplace=True)
+        df_stock = df_stock.melt(id_vars='Date', var_name='Tickers', value_name='Adj_Close')
+
         end = time.time()
         print("Elapsed Time Seq: " + str(end - start))
 
-        returns_stocks = df_stock.pct_change()
-        returns_stocks = returns_stocks[1:]
-        returns_stocks_acm = (1 + returns_stocks).cumprod()
+        # returns_stocks = df_stock.pct_change()
+        # returns_stocks = returns_stocks[1:]
+        # returns_stocks_acm = (1 + returns_stocks).cumprod()
 
-        print(returns_stocks_acm)
+        # print(returns_stocks_acm)
 
-        returns_stocks_acm.to_csv("./data/stocks_s&p_test.csv")
+        df_stock.to_csv("./data/stocks_indicators.csv", index=False)
+    
 
-    def calculate_volatility()
+    def calculate_volatility(self, dataStock) -> None:
+        self.dataDir = dataStock
+        df = pd.read_csv(dataStock, sep=",")
+
+        stock1_return = df['Adj_Close'].pct_change()
+        returns_stock1 = stock1_return[1:]
+        returns_stock1_acm = (1+returns_stock1).cumprod()
+
+        df['Volatility'] = returns_stock1_acm
+
+        df.to_csv("./data/stocks_indicators.csv", index=False)
 
 
-# gerar dataframes para cada um dos metodos --> onde cada linha é um ticker
-# e os metodos abaixo seriam as colunas do df
+
+    
+    # def calculate_standard_deviation() -> None:
+    #     pass
+
+    # def calculate_volatility_portfolio(stocks_list) -> None:
+    #     cov_matrix = pd.concat(stocks_list, axis=1).cov()
+
+
+    #     portfolio_return = (0.5 * returns_stock1) + (0.5 * returns_stock2)
+    #     portfolio_return_acm = (0.5 * returns_stock1_acm) + (0.5 * returns_stock2_acm)
+
+
+    #     print('Retorno de ' + stock1 + ': ', returns_stock1_acm[-1])
+    #     print('Retorno de ' + stock2 + ': ', returns_stock2_acm[-1])
+    #     print('Retorno do portfolio: ', portfolio_return_acm[-1])
+
+    
+        
+
+
+# gerar dataframes para cada um dos metodos --> onde cada linha é um ticker e os metodos abaixo seriam as colunas do df
+# prefiro gerar um df diferente para cada metodo calculado e depois ficar mergiando df por datas
 
 # metodo para calculo da volatilidade (desvio padrao)
 # metodo para retorno medio diario (tirar a media do retorno)
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from get_data import GetData
+import pandas as pd
 
 dataDir = "data/index_sample.csv"
 
-data = GetData("01/12/2020", "05/07/2023")
+data = GetData("01/12/2021", "05/07/2023")
 data.import_data(dataDir)
 
+
+stock_data = "data/stocks_indicators.csv"
+
+
 # generate dataframe volatility
-data.calculate_voltatility()
+data.calculate_volatility(stock_data)
+
+# df = pd.read_csv('data/stocks_s&p_test.csv')
+# print(df.head())
